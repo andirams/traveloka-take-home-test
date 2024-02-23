2bc3cc79a1a83caca751eed9f89e1d23a91d1750
Andira Mahendra S <84491959+andirams@users.noreply.github.com>
2024-02-23 20:38:02 +0700

    update-all
    update all fix

---
+++ web-automation/cypress/e2e/SelectCars/helper/locator.cy.js
@@ -0,0 +1,42 @@
+export class Locator {
+	// input
+    inputNotes = '.notes';
+    inputContactDetail = '.contact-detail';
+    inputDriverDetail = '.driver-detail';
+    inputPickUpDate = '.input-pickup-date';
+    inputPickUpTime = '.input-pickup-time';
+    inputDropOffDate = '.input-dropoff-date';
+    inputDropOffTime = '.input-dropoff-time';
+    inputSpecialReq = '.input-special-req';
+
+	// button
+    btnSearchCar = '.search-car';
+    btnContinuePD = '.continue-pd';
+    btnBookNow = '.book-now';
+    btnContinue = '.continue';
+
+	// select
+    selectCarProduct = '.cars-product';
+	selectTabWithoutDriver = '.without-driver';
+	selectPickUpLoc = '.pickup-loc';
+	selectPickUpDate = '.pickup-date';
+	selectDropOffDate = '.dropoff-date';
+    selectCar = '.select-car';
+    selectCarProv = '.select-car-prov';
+    selectPickUpLocRent = '.pickup-loc-rent';
+    selectDropOffLocOth = '.pickup-loc-rent';
+    selectPaymentMethod = '.payment-method';
+
+	// title
+	titleCarsProduct = '.cars-product1';
+
+    // requirement
+    req1 = 'req-1';
+    req2 = 'req-2';
+    req3 = 'req-3';
+    req4 = 'req-4';
+
+    // alert
+    paymentSuccess = '.payment-success';
+    paymentFailed = '.payment-failed';
+}
